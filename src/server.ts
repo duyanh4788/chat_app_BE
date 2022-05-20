1da96d11cba4d79a5edcec784f31d87aae10aa74
duyanhvu <anh.vu@tekmedi.vn>
2022-05-20 15:12:17 +0700

    config url server

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import app from './app/app';
 const PORT: string | number | any = process.env.PORT_SERVER || 5000;
 const HOST: string = '0.0.0.0';
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running API on port : ${PORT}`);
-});
+app
+  .get('/', () => {
+    console.log('Server is Running ...');
+  })
+  .listen(PORT, HOST, () => {
+    console.log(`Running API on port : ${PORT}`);
+  });
