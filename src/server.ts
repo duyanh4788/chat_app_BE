e5bc0d13019ec37cf594055520721fa3fe5fb615
duyanhvu <anh.vu@tekmedi.vn>
2022-05-20 15:25:30 +0700

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config();
 import app from './app/app';
+import { Request, Response } from 'express';
 
 const PORT: string | number | any = process.env.PORT_SERVER || 5000;
 const HOST: string = '0.0.0.0';
 
-app
-  .get('/', () => {
-    console.log('Server is Running ...');
-  })
-  .listen(PORT, HOST, () => {
-    console.log(`Running API on port : ${PORT}`);
-  });
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is Running ...');
+});
+app.listen(PORT, HOST, () => {
+  console.log(`Running API on port : ${PORT}`);
+});
