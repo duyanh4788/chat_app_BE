092b50b185c7fc0392ea8ddafe39c1382db6f043
duyanh4788 <duyanh4788@gmail.com>
2023-03-28 00:43:08 +0700

    config cors

--- src/app/App.ts
+++ src/app/App.ts
@@ -39,10 +39,7 @@ class App {
 
   public configCors(): void {
     this.App.use(seesion(sessionOptions));
-    this.App.use(cors({
-      origin: process.env.END_POINT_HOME,
-      credentials: true
-    }));
+    this.App.use(cors());
   }
 
   public configJson(): void {
